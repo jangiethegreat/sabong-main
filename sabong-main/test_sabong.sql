f2793d69a2d579fb10a0b334e58a33d723235681
John Cris Fernando <118041167+jangiethegreat@users.noreply.github.com>
2023-08-13 17:30:35 +0800

    sql commit

--- sabong-main/test_sabong.sql
+++ sabong-main/test_sabong.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 19, 2023 at 09:55 AM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.0.23
+-- Generation Time: Aug 13, 2023 at 11:29 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,17 +39,62 @@ CREATE TABLE `events` (
   `total_event_fights` int(50) UNSIGNED NOT NULL,
   `expected_event_fights` int(50) UNSIGNED NOT NULL,
   `event_status` varchar(288) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `events`
 --
 
 INSERT INTO `events` (`id`, `event_name`, `event_area`, `event_date`, `meron_position`, `wala_position`, `liamado_plasada`, `dehado_plasada`, `total_event_fights`, `expected_event_fights`, `event_status`) VALUES
-(12, 'bulacan-1', 'San Ildenfonso', '2023-07-01', 'kaliwa', 'kanan', '12', '6', 20, 304, 'Close'),
-(13, '2-hits', 'San Ildenfonso', '2023-07-19', 'Kaliwa', 'Kanan', '10', '10', 0, 10, 'Close'),
-(14, 'bulacan-1', 'San Ildenfonso', '2023-07-19', 'Kaliwa', 'Kanan', '5', '5', 0, 10, 'Open'),
-(17, 'bulacan-1', 'San Ildenfonso', '2025-10-01', 'kaliwa', 'kanan', '8', '9', 0, 304, 'Open');
+(51, '2-hits', 'bulacan', '2023-08-13', 'Kanan', 'Kaliwa', '5', '5', 0, 20, 'Close'),
+(52, '2-hits', 'bulacan', '2023-08-14', 'Kanan', 'Kaliwa', '6', '4', 0, 3, 'Open');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `fights`
+--
+
+CREATE TABLE `fights` (
+  `id` int(20) NOT NULL,
+  `event_id` int(20) NOT NULL,
+  `fight_no` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `meron_total_bet` int(11) DEFAULT NULL,
+  `wala_total_bet` int(11) DEFAULT NULL,
+  `plasada_total` int(11) DEFAULT NULL,
+  `winner` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `fights`
+--
+
+INSERT INTO `fights` (`id`, `event_id`, `fight_no`, `date`, `meron_total_bet`, `wala_total_bet`, `plasada_total`, `winner`, `status`) VALUES
+(119, 51, 1, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(120, 51, 2, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(121, 51, 3, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(122, 51, 4, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(123, 51, 5, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(124, 51, 6, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(125, 51, 7, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(126, 51, 8, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(127, 51, 9, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(128, 51, 10, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(129, 51, 11, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(130, 51, 12, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(131, 51, 13, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(132, 51, 14, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(133, 51, 15, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(134, 51, 16, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(135, 51, 17, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(136, 51, 18, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(137, 51, 19, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(138, 51, 20, '2023-08-13', NULL, NULL, NULL, NULL, 'pending'),
+(139, 52, 1, '2023-08-14', NULL, NULL, NULL, NULL, 'pending'),
+(140, 52, 2, '2023-08-14', NULL, NULL, NULL, NULL, 'pending'),
+(141, 52, 3, '2023-08-14', NULL, NULL, NULL, NULL, 'pending');
 
 --
 -- Indexes for dumped tables
@@ -62,6 +107,12 @@ ALTER TABLE `events`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `fights`
+--
+ALTER TABLE `fights`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -69,7 +120,13 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+
+--
+-- AUTO_INCREMENT for table `fights`
+--
+ALTER TABLE `fights`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=142;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
